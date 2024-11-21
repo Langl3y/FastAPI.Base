d3249c9918ca26653c223a45cb249a1ba4aaf5a1
Tom Ho <lanly@Toms-MacBook-Pro.local>
2024-11-21 22:50:17 +0700

    add password hashing and jwt utilities

--- api/common/utils.py
+++ api/common/utils.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+import bcrypt
+import jwt
+from datetime import datetime, timedelta, timezone
+from typing import Union
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from be.env import *
@@ -25,3 +30,29 @@ def create_database():
     Create all tables from defined models in api.models
     """
     Base.metadata.create_all(bind=engine)
+
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt(rounds=10)
+    hashed_password = bcrypt.hashpw(password.encode(), salt).decode()
+
+    return hashed_password
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret, algorithm=algorithm)
+    return encoded_jwt
+
+
+def decode_jwt(access_token: str):
+    try:
+        payload = jwt.decode(access_token, secret, algorithms=[algorithm])
+        return payload
+    except jwt.InvalidTokenError as e:
+        return e
