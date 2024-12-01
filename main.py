a5d1d315dfe9340adcc365f2654af59f7c7d5461
Langl3y <hieuhovan954@gmail.com>
2024-12-01 20:49:08 +0700

    add CORS middleware

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers import tasks_router, users_router, weather_info_router, login_router
 from be.env import allowed_hosts
 
 app = FastAPI()
-app.add_middleware(TrustedHostMiddleware, allowed_hosts=allowed_hosts)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],  # Include OPTIONS
+    allow_headers=["*"],
+)
 
 # Include routers
 
