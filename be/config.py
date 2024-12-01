9e8db10f58d2e277f550d1cba108e682c95d413a
Langl3y <hieuhovan954@gmail.com>
2024-12-01 17:21:08 +0700

    create admin user if not exists in DB

--- be/config.py
+++ be/config.py
@@ -3,15 +3,19 @@ from api.common import SessionLocal
 from api.common.utils import hash_password
 from env import admin_password
 
-db = SessionLocal()
 
-admin = User(
-    username="admin",
-    password=hash_password(admin_password),
-    role="admin"
-)
+def create_admin():
+    db = SessionLocal()
 
-db.add(admin)
-db.commit()
-db.refresh(admin)
-db.close()
+    admin_user = db.query(User).filter(User.username == "admin").first()
+    if not admin_user:
+        admin = User(
+            username="admin",
+            password=hash_password(admin_password),
+            role="admin"
+        )
+
+        db.add(admin)
+        db.commit()
+        db.refresh(admin)
+        db.close()
